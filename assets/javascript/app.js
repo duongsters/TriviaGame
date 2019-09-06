4f221210f0c400b4243d57b3352a513e26dc8686
Andrew Duong <duongsters@gmail.com>
2019-09-06 09:06:36 -0700

    added a pseudocode outline of what i plan on doing for my app.js and index.html files

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+//-------- global variables--------------
+
+
+// ----------questions/answers array index---------
+
+
+
+// -------button click DOM-------------
+
+
+
+//--------restart/refresh TriviaGame rendering------
+
+
+
+//-----creating Questions & Answers rendering-----
+
+
+
+
+//--------timer countdown rendering-------
+
+
+
+//------trivia scoreboard rendering-----
+
+
+
+
+
+
+
+
