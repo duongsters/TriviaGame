cd4c3fc7fd23eaa0fd9364730172a5f9c847d1a5
Andrew Duong <duongsters@gmail.com>
2019-09-08 19:23:27 -0700

    finished up my index.html and started working on my app.js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,62 @@
 //-------- global variables--------------
-
+var correctAnswer = 0;
+var incorrectAnswer = 0;
+var currentQ;
+var remainingQ;
+var time;
+var sec;
+var userChoice;
+var txtMsg = {
+    correct: "Ding, ding! Correct answer choice!",
+    wrong: "Whoops--incorrect answer!",
+    noTime:"AH! Out of time!",
+    lastQ: "You answered all the questions--time to tally up the scores"
+}
 
 // ----------questions/answers array index---------
+var triviaQ = [{
+    question: "What year did Volkswagen introduce the Golf GTI model?",
+    answerChoices: ["1976", "1965", ],
+    answer: 0
+},{
+    question: "The original Legoland opened in which country in 1968?",
+    answerChoices: ["USA", "Denmark","Sweden", "Portugal"],
+    answer: 1
+},{
+    question:"Who did sports fans know as 'the Wizard of Westwood'?",
+    answerChoices:["John Wooden", "Bob Knight", "Bill Walton", "Kareem Abdul Jabbar"],
+    answer: 0
+},{
+    question:"Triple Sec is a clear liqueur of what flavor?",
+    answerChoices:["Cherries", "Apples", "Oranges", "Watermelon"],
+    answer: 2
+},{
+    question:"What is traditionally the main fruit ingredient in Black Forest cake?",
+    answerChoices:["Oranges", "Black Berries", "Cherries", "Apples"],
+    answer: 2
+},{
+    question: "What type of foodstuff is traditionally eaten on Shrove Tuesday?",
+    answerChoices:["Corned Beef", "Sausage Links", "French Toast", "Pancakes"],
+    answer: 3
+},{
+    question:"What do you get when you mix whiskey, sweet vermouth and angostura bitters?",
+    answerChoices:["Bloody Mary", "Manhatten", "Gin & Tonic", "Raging Bull"],
+    answer: 1
+},{
+    question:"What was the first sport to be televised in the US?",
+    answerChoices:["Baseball", "Football", "Basketball", "Soccer"],
+    answer: 0
+},{
+    question:"What car company introduced the Pinto in the 1970s?",
+    answerChoices:["Volvo", "Volkswagon", "Chevrolet", "Ford Motor Company"],
+    answer: 3
+},{
+    question: "Bill Gates dropped out of which of educational institution?",
+    answerChoices:["Stanford", "Princeton", "Harvard", "Yale"],
+    answer:  2
+},
 
+]
 
 
 // -------button click DOM-------------
